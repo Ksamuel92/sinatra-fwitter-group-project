9e770d52771b813cf2a51306c57d77028867e9d8
Kyle Samuel <Ksamuel92@gmail.com>
2021-05-29 16:24:05 -0400

    work on /tweets/show.erb, migrated tables in default env

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -46,9 +46,10 @@ patch  '/tweets/:id' do
     redirect to "/tweets/#{tweet.id}"
 end
 
-delete '/tweets/:id' do
+delete '/tweets/:id/delete' do
     tweet = Tweet.find_by_id(params[:id])
     tweet.delete
     redirect to "/tweets"
+end
 end
 
