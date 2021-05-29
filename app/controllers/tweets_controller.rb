71df1747e21669de8809930d4b9c8c78db6c99bb
Kyle Samuel <Ksamuel92@gmail.com>
2021-05-29 15:48:29 -0400

    Create index routes in app and tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
 
+get '/tweets' do
+    if !logged_in?
+        redirect to '/login'
+    end
+    @user = User.find(session[:user])
+    @tweets = Tweet.all
+    erb :"tweets/index"
+end
 
 end
