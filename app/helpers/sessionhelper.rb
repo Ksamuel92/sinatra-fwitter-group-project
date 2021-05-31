1dc95a4eafcbdec133e409548dff81d2a2579379
Kyle Samuel <Ksamuel92@gmail.com>
2021-05-31 11:14:10 -0400

    made sessionhelper methods class methods

--- app/helpers/sessionhelper.rb
+++ app/helpers/sessionhelper.rb
@@ -1,10 +1,10 @@
 class SessionHelper 
-    def current_user(session)
+    def self.current_user(session)
         user = User.find_by_id(session[:user_id])
         user
     end
 
-    def is_logged_in?(session)
+    def self.is_logged_in?(session)
         !!session[:user_id]
     end
 end
