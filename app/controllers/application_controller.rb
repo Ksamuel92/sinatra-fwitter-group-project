38b963db4c4523b1302f84282ba6021e9d51a377
Kyle Samuel <Ksamuel92@gmail.com>
2021-05-31 11:13:36 -0400

    create session views, tweet index, helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,33 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "session_encryption"
   end
 
   get '/' do
     
     erb :"index"
   end
+
+  get '/signup' do
+    
+    erb :"sessions/signup"
+    end
+
+    post '/signup' do
+      user = User.new(params)
+      if !user.save
+        redirect to '/signup'
+      end
+      user.save
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    end
+
 
 end
